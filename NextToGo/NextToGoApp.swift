6b05ce24b826e02a35595f698b8649f7dc2bf0d8
Chris Nevin <cjnevin@protonmail.com>
2025-05-19 19:18:30 +1000

    App: Added main NextToGo app

---
+++ NextToGo/NextToGoApp.swift
@@ -0,0 +1,18 @@
+//
+//  NextToGoApp.swift
+//  NextToGo
+//
+//  Created by Chris Nevin on 18/5/2025.
+//
+
+import RacesFeature
+import SwiftUI
+
+@main
+struct NextToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NextRacesView()
+        }
+    }
+}
